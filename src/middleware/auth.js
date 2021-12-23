01da603ad3f007cfa5e4de877597663f8f2a77a6
chakrd <godlooter@gmail.com>
2021-12-24 01:55:05 +0700

    authfix

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -8,7 +8,7 @@ const auth = async (req, res, next) => {
     try {
 
         const token     = req.header('Authorization').replace('Bearer ', '')
-        const decoded   = jwt.verify(token, 'thisismysecrete')
+        const decoded   = jwt.verify(token, process.env.JWT_SECRET)
         const user      = await User.findOne({ _id: decoded._id, 'tokensArray.token': token })
         
 
